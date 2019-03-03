ef8e02a5f275e4ffc1b55ddda548a1817ac96dd6
Ronak Patel <ronak1.patel@ryerson.ca>
2019-03-03 15:41:20 -0500

    created layout for each song with its song name and a select button. imported selectSong from actions folder such that each time a user clicks the select button beside the song, the selectedSong function will update accordingly.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import SongList from "./SongList";
 
 // import a named export from a file
 import { selectSong } from "../actions";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
+// 3rd party dependencies
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+// local files
+import App from "./components/App";
+import reducers from "./reducers";
+
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
