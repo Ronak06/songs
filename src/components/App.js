92953e12c1bb8c2674e9f21e3ba16f6bded96325
Ronak Patel <ronak1.patel@ryerson.ca>
2019-03-06 21:35:12 -0500

    created SongDetail component to display the details of a song a user selects.

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import SongList from "./SongList";
-
-// import a named export from a file
-import { selectSong } from "../actions";
+import SongDetail from "./SongDetail";
 
 const App = () => {
   return (
@@ -10,6 +8,9 @@ const App = () => {
       <div className="ui row">
         <div className="column eight wide">
           <SongList />
+        </div>
+        <div className="column eight wide">
+          <SongDetail />
         </div>
       </div>
     </div>
